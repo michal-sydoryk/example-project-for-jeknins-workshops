cf67379d8491f2a00997a8112e4280aa865c79d1
Michał Sydoryk <sydoryk.m@gmail.com>
2020-05-25 11:06:31 +0200

    Commit MS

--- src/main/java/com/epam/q3ap/jenkinsworkshops/Example.java
+++ src/main/java/com/epam/q3ap/jenkinsworkshops/Example.java
@@ -9,6 +9,8 @@ package com.epam.q3ap.jenkinsworkshops;
  */
 class Example {
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    
+	  System.out.println("Hello world");
+	  System.out.println("Hello world 2");
   }
 }
